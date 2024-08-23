b143a7e24c4a86efc6c855f0cf961efab42a6554
Tayofi <Kinges@s.dcsdk12.org>
2024-08-23 15:45:59 -0600

    the commit is good

---
+++ scripts/variables.js
@@ -0,0 +1,19 @@
+let intelligenceQuotient = 100;
+//this varible is a number
+let firstName = "wawaCat";
+let lastName = "hehehe >:)";
+console.log(firstName, lastName, "has IQ", intelligenceQuotient);
+let myPet = {
+    type: "floofer",
+    Name: "zues",
+    ageInHUmanYears: "5",
+    primColor: "brown", 
+secColor: "white"
+};
+// yippee object waaaaw
+console.log("the guy who lives rent free in my house looks like this", myPet)
+//arrays are fun >:(
+let favIcecream = [
+    "mintChocalateChip"
+];
+console.log("My favorite icecream is", favIcecream);
