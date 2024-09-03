563a773959f620f1b50d1e02499c5fa63334832b
Tayofi <Kinges@s.dcsdk12.org>
2024-09-03 16:21:15 -0600

    tuesday commit

---
+++ scripts/conditionals.js
@@ -0,0 +1,13 @@
+//@ts-check
+
+let isTheSKyBlue=true;
+let isTheSunUp=true;
+if(isTheSKyBlue) {let grassColor="light Green"
+    console.log("the sky is indeed blueand the grass is", grassColor)
+
+}else if(!isTheSunUp){let grassColor="Dark Green"
+    console.log("it might be night and the grasss is", grassColor)
+
+}else{let grassColor= "medium green"
+    console.log("it might be raining or stormyand the grass is", grassColor)};
+console.log(isTheSKyBlue, isTheSunUp);
