f740de1ac6c8861d7d6201ead5f4f82f1c8ee127
Tayofi <Kinges@s.dcsdk12.org>
2024-09-06 15:42:07 -0600

    qommit

--- scripts/rps-game.js
+++ scripts/rps-game.js
@@ -5,18 +5,9 @@ let btnRock = document.getElementById("btn-rock");
 let btnPaper = document.getElementById("btn-paper");
 let btnscissors = document.getElementById("btn-scissors");
 
-function rockClicked(name) {
-console.log("Rock was selected")
-};
-btnRock?.addEventListener("click", rockClicked);
 
-function scissorsClicked(name) {
-console.log("Scissors was selected")
-};
-btnscissors?.addEventListener("click", scissorsClicked);
 
-    function paperClicked(name) {
-console.log("Paper was selected")
-};
-btnPaper?.addEventListener("click", paperClicked)
-    
+function playerControlHandler(evente){
+    console.log(evente.target.innerText);
+}
+    playerControls?.addEventListener("click", playerControlHandler);
