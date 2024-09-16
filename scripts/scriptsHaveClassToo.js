383cd4ad40481d0c224924a0f3579048e7a8a922
Tayofi <Kinges@s.dcsdk12.org>
2024-09-16 16:21:57 -0600

    No cheese, cheesepuffs

---
+++ scripts/scriptsHaveClassToo.js
@@ -0,0 +1,18 @@
+class CheesyDibbles {
+    constructor(cheesiness) {
+this.type = cheesiness;
+this.brand = "fritoLay";
+this.cost = 2.3;
+this.age = 83;
+ }
+
+ eat() {
+    console.log(`Chomp chomp chomp ${this.cheesiness}, is expired >:(`)
+ }
+}
+let cheesePuffs = new CheesyDibbles("cheesePuffs");
+cheesePuffs.brand = "cheesePuffBrand";
+cheesePuffs.age = 0.2;
+
+let cheetoes = new CheesyDibbles("cheetoes");
+cheetoes.age = 3000;
