07907b406989d81510740a9fd8566474d5d6381b
Tayofi <Kinges@s.dcsdk12.org>
2024-09-18 16:21:18 -0600

    my circle is a little bit big, also happy face!

--- scripts/canvas-basics/app.js
+++ scripts/canvas-basics/app.js
@@ -1,8 +1,18 @@
 //@ts-check
 /** @type {HTMLCanvasElement} */
 //@ts-ignore canvas is an HTMLCanvasElement
-const canvas = document.getElementById("game-canvas");
+const canvas = document.getElementById("bideo-game");
 /** @type {CanvasRenderingContext2D} */
 //@ts-ignore ctx is an canvasrenderingcontext2d
 const ctx = canvas.getContext("2d");
 
+ctx.fillRect(0, 0,50,100);
+ctx.fillRect(200, 0,50,100);
+ctx.fillRect(0, 300,250,50);
+ctx.fillRect(0, 250,50,50);
+ctx.fillRect(200, 250,50,50);
+
+
+ctx.beginPath();
+ctx.arc(500,500, 250, 0, Math.PI * 2);
+ctx.fill();
