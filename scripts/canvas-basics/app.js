ab9b5483aeea626e10b729242f8e08e1b079c486
Tayofi <Kinges@s.dcsdk12.org>
2024-09-26 16:20:44 -0600

    two in one day?(gasp) I've never heard of such a thing!

--- scripts/canvas-basics/app.js
+++ scripts/canvas-basics/app.js
@@ -1,4 +1,5 @@
 //@ts-check
+import { Squaare } from "./shapes/square.js";
 /** @type {HTMLCanvasElement} */
 //@ts-ignore canvas is an HTMLCanvasElement
 const canvas = document.getElementById("bideo-game");
@@ -6,54 +7,10 @@ const canvas = document.getElementById("bideo-game");
 //@ts-ignore ctx is an canvasrenderingcontext2d
 const ctx = canvas.getContext("2d");
 
-class Squaare {
-    constructor(x, y) {
-this.x=x;
-this.y=y;
-
-this.width=50;
-this.height=this.width;
-this.hue=0;
-this.speedMult = 2
-this.speedX=Math.floor(Math.random()*this.speedMult) + 1;
-
-this.speedY=Math.floor(Math.random()*this.speedMult) + 1;
-this.dirX = 1;
-this.dirY = 1;
-    }
-    update(){
-        this.x += this.speedX * this.dirX;
-        this.y += this.speedY * this.dirY;
-        this.hue++;
-
-        if(this.hue > 360){
-            this.hue=0;
-        }
-        if(this.x<0){
-            //off screen - left side: move right
-this.dirX=1;
-        }
-        else if(this.x+this.width>canvas.width){
-            //offscreen right: move left
-            this.dirX=-1;
-        };
-        if(this.y<0){
-            //off screen - left side: move right
-this.dirY=1;
-        }
-        else if(this.y+this.height>canvas.height){
-            //offscreen right: move left
-            this.dirY=-1;
-        };
-    };
-    draw (){
-        ctx.fillStyle = `hsla(${this.hue}, 100%, 78%, 100%`
-        ctx.fillRect(this.x, this.y,this.width,this.height);   
-    };
-}
 
 
-let s1 = new Squaare(0,0);
+
+let s1 = new Squaare(0,0, ctx, canvas);
 let shapes = [];
 for (let i = 0; i < 100; i++){
     shapes.push(new Squaare(0,0));
